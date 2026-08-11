f15d75c7c8177574107d4b3a2a86150758cb2688
JP Lataquin <jp.lataquin@gmail.com>
2026-08-11 16:12:41 +0800

    fix:

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,7 +52,7 @@
                                     <a class="nav-link" href="{{ route('warehouses.index') }}">Warehouses</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('assignments.index') }}">Logger Assignments</a>
+                                    <a class="nav-link" href="{{ route('assignments.index') }}">Assign</a>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link d-flex align-items-center gap-1" href="{{ route('items.index') }}">
